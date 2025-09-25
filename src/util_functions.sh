e308263dc0f87af9837b2c7edfecadaba4e99633
Sifl46 <77085292+Sifl46@users.noreply.github.com>
2025-09-25 08:25:39 -0700

    Update util_functions.sh
    Signed-off-by: Sifl46 <77085292+Sifl46@users.noreply.github.com>

--- src/util_functions.sh
+++ src/util_functions.sh
@@ -480,7 +480,7 @@ function check_toml_env() {
 
 function supported_tools() {
   local arg="${1:-}"
-  local tools=("avbroot" "afsr" "alterinstaller" "custota" "custota-tool" "msd" "bcr" "oemunlockonboot" "my-avbroot-setup")
+  local tools=("avbroot" "afsr" "custota" "custota-tool" "oemunlockonboot" "my-avbroot-setup")
 
   if [[ "${arg}" == "cdd" ]]; then
     echo "${tools[@]}"
