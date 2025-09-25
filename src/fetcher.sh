c23d2e38fc48a3da8331964dc8622c7006c3bb75
Sifl46 <77085292+Sifl46@users.noreply.github.com>
2025-09-25 10:19:52 -0700

    Update fetcher.sh
    Signed-off-by: Sifl46 <77085292+Sifl46@users.noreply.github.com>

--- src/fetcher.sh
+++ src/fetcher.sh
@@ -7,7 +7,7 @@ source src/declarations.sh
 # Fetch the latest version of GrapheneOS and Magisk and sets up the OTA URL
 function get_latest_version() {
   local latest_grapheneos_version=$(curl -sL "${GRAPHENEOS[OTA_BASE_URL]}/${DEVICE_NAME}-${GRAPHENEOS[UPDATE_CHANNEL]}" | sed 's/ .*//')
-    if [[ "${KITSUNE}" == 'true' ]]; then
+    if [[ "${KITSUNE}" == "true" ]]; then
       local latest_magisk_version=$(
         git ls-remote --tags "${DOMAIN}/${KITSUNEMAGISK[REPOSITORY]}.git" |
           awk -F'\t' '{print $2}' |
